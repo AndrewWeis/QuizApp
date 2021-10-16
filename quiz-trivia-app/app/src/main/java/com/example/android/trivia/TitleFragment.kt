ff176f3f937b0d906e9e45717b6194b233ed9da5
Andrew Weiss <andreivayss@gmail.com>
2021-10-16 22:25:12 +0700

    Added nav graph logic

--- quiz-trivia-app/app/src/main/java/com/example/android/trivia/TitleFragment.kt
+++ quiz-trivia-app/app/src/main/java/com/example/android/trivia/TitleFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.example.android.trivia.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment(R.layout.fragment_title) {
@@ -20,6 +21,9 @@ class TitleFragment : Fragment(R.layout.fragment_title) {
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_title, container, false)
 
+        binding.playButton.setOnClickListener { view: View ->
+            Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_gameFragment)
+        }
 
         return binding.root
     }
