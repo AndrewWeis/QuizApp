d6fda3cb14f6b53e7f5ea2b70094d8e0dcf25e76
Andrew Weiss <andreivayss@gmail.com>
2021-10-17 10:19:18 +0700

    Adding menu

--- quiz-trivia-app/app/src/main/java/com/example/android/trivia/TitleFragment.kt
+++ quiz-trivia-app/app/src/main/java/com/example/android/trivia/TitleFragment.kt
@@ -1,12 +1,12 @@
 package com.example.android.trivia
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.android.trivia.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment(R.layout.fragment_title) {
@@ -25,6 +25,18 @@ class TitleFragment : Fragment(R.layout.fragment_title) {
             Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_gameFragment)
         }
 
+        setHasOptionsMenu(true)
+
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.overflow_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item,
+            view!!.findNavController()) || super.onOptionsItemSelected(item)
     }
 }
